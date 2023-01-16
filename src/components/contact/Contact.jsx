041bd1c8b323ff103d129e81acdeaf039a5c4caf
AdarshVMore <skullcrushermore@gmail.com>
2023-01-16 15:18:06 +0530

    Contact completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 import "./contact.css";
+import { GrMail } from "react-icons/gr";
+import { BsWhatsapp } from "react-icons/bs";
 
 function Contact() {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    emailjs
+      .sendForm(
+        "service_xsv7shd",
+        "template_3xsprxd",
+        form.current,
+        "6fo4x42aWSS_lT_RE"
+      )
+      .then(
+        () => {
+          alert("email sent Succesfully ✅");
+        },
+        (error) => {
+          alert(error.text);
+        }
+      );
+  };
+  return (
+    <section id="contact">
+      <h5>Got Work for me</h5>
+      <h2>Contact Me</h2>
+
+      <div className="contact_container container">
+        <div className="contact_option">
+          <article>
+            <GrMail className="contact_icon" />
+            <h3>Mail</h3>
+            <h5>adarshv.more18@gmail.com</h5>
+            <a href="mailto:adarshv.more18@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article>
+            <BsWhatsapp className="contact_icon" />
+            <h3>Whatsapp</h3>
+            <h5>+91 9067684680</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+919067684680"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="text" name="email" placeholder="Your Email Id" />
+          <textarea
+            name="message"
+            rows="10"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./footer.css";
+import { BsTwitter, BsInstagram, BsFacebook } from "react-icons/bs";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className=" footer_container">
+      <a href="#">
+        <h2>ADARSH MORE</h2>
+      </a>
+      <div className="nav_links">
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <div className="socials_icons">
+        <a href="">
+          <BsFacebook />
+        </a>
+        <a href="https://www.instagram.com/adarsh_more19">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com/Adarsh_VMore" target="blank">
+          <BsTwitter />
+        </a>
+      </div>
+      <h5 className="copyright">&copy; Adarsh More all rights reserved </h5>
+    </footer>
+  );
 }
 
 export default Footer;
